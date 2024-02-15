11d76c6f00c8796668a250b03fb0c19b8f82716d
Cedy <58510598+cedylad@users.noreply.github.com>
2024-02-15 11:33:17 +0100

    UPDATE connexion with API

--- components/connexionAccount.js
+++ components/connexionAccount.js
@@ -1,8 +1,35 @@
+import { useState } from "react";
 import styles from "@/styles/components/ConnexionAccount.module.css";
 import Head from "next/head";
 import Link from "next/link";
 
 export default function ComponentConnexion() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleLogin = async () => {
+    try {
+      const response = await fetch(
+        "https://famdev.srvkoikarpfess.ddns.net/api/endpoints/loginEND",
+        {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({ email, password }),
+        }
+      );
+
+      if (response.ok) {
+        console.log("Connexion réussie !");
+      } else {
+        console.error("Échec de la connexion");
+      }
+    } catch (error) {
+      console.error("Erreur lors de la connexion:", error);
+    }
+  };
+
   return (
     <>
       {/*  Metadata */}
@@ -20,16 +47,27 @@ export default function ComponentConnexion() {
           </Link>
           <p className={styles.connexion}>Connexion</p>
           <label>
-            <input placeholder="Nom d'utilisateur" />
+            <input
+              placeholder="Nom d'utilisateur"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+            />
           </label>
           <label>
-            <input type="password" placeholder="Mot de passe" />
+            <input
+              type="password"
+              placeholder="Mot de passe"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+            />
           </label>
           <label className={styles.checkbox}>
             <input type="checkbox" />
             Se souvenir de moi
           </label>
-          <div className={styles.button}>Se connecter</div>
+          <div className={styles.button} onClick={handleLogin}>
+            Se connecter
+          </div>
           <Link className={styles.linkConnexion} href="/">
             Annuler, revenir à l'accueil
           </Link>
--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,4 @@
 import Head from "next/head";
-import Image from "next/image";
 import { Inter } from "next/font/google";
 import styles from "@/styles/Home.module.css";
 
@@ -14,9 +13,7 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <main className={`${styles.main} ${inter.className}`}>
-        hello wordl home page
-      </main>
+      <main>hello wordl home page</main>
     </>
   );
 }
