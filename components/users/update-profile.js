428485badb2062218ed520377a50d3f5f3e3dcf1
Antohnyh <anthony.hanen@viacesi.fr>
2024-05-14 05:21:20 +0200

    mise a jour update

--- components/users/update-profile.js
+++ components/users/update-profile.js
@@ -1,6 +1,17 @@
 import {useEffect, useState} from "react";
 import styles from "@/styles/components/UpdateProfile.module.css";
-import process from "next/dist/build/webpack/loaders/resolve-url-loader/lib/postcss";
+import {redirect} from "next/navigation";
+
+function getCookie(name) {
+	const cookies = document.cookie.split(';');
+	for (let i = 0; i < cookies.length; i++) {
+		const cookie = cookies[i].trim();
+		if (cookie.startsWith(name + '=')) {
+			return cookie.substring(name.length + 1);
+		}
+	}
+	return null;
+}
 
 export default function UpdateProfileForm({
 	user
@@ -8,7 +19,38 @@ export default function UpdateProfileForm({
 
 	useEffect(() => {
 		setToken(localStorage.getItem("token"))
+
+		const getUser = async () => {
+			const endpoint = process.env.NEXT_PUBLIC_API_ENDPOINT +  "users?id=" + getCookie('userId');
+
+			const res = await fetch(endpoint, {
+				method: 'GET',
+				headers: {
+					Accept: 'application/json',
+					Authorization: 'Bearer ' + token
+				}
+			});
+			const user = await res.json();
+			console.log(user);
+			return user;
+		};
+		// getUser()
+
 	}, []);
+
+	const [token, setToken] = useState('')
+	const [userId, setUserId] = useState('')
+
+	const [dateNaissance, setDateNaissance] = useState(user.utilisateur_datenaissance)
+	const [codePostal, setCodePostal] = useState(user.utilisateur_code_postal)
+	const [prenom, setPrenom] = useState(user.utilisateur_prenom)
+	const [nom, setNom] = useState(user.utilisateur_nom)
+	const [numRue, setNumRue] = useState(user.utilisateur_numero_rue)
+	const [ville, setVille] = useState(user.utilisateur_ville)
+	const [pays, setPays] = useState(user.utilisateur_pays)
+	const [rue, setRue] = useState(user.utilisateur_rue)
+
+	const [message, setMessage]= useState('')
 
 	const handleSubmit = async (e) => {
 		e.preventDefault()
@@ -24,7 +66,7 @@ export default function UpdateProfileForm({
 			"utilisateur_rue": rue
 		}
 
-		const res = await fetch(process.env.API_ENDPOINT + "/users", {
+		const res = await fetch(process.env.API_ENDPOINT + "/users" + userId, {
 			method: "PATCH",
 			headers: {
 				"Content-Type": "application/json",
@@ -32,54 +74,50 @@ export default function UpdateProfileForm({
 			},
 			body: JSON.stringify(data)
 		})
-	}
 
-	const [dateNaissance, setDateNaissance] = useState(user.datenaissance)
-	const [codePostal, setCodePostal] = useState(user.codepostal)
-	const [prenom, setPrenom] = useState(user.utilisateur_prenom)
-	const [nom, setNom] = useState(user. utilisateur_nom)
-	const [numRue, setNumRue] = useState(user.numerorue)
-	const [ville, setVille] = useState(user.ville)
-	const [pays, setPays] = useState(user.pays)
-	const [rue, setRue] = useState(user.rue)
-	const [token, setToken] = useState('')
+		if (res.ok) {
+			setMessage('Mise à jour avec succès.')
+		} else {
+			setMessage('Erreur lors de la mise à jour.')
+			//TODO gestion erreur
+		}
+	}
 
 	return (
 		<div className={styles.wrapper}>
 			<h1 className={styles.titre}>Mon profil</h1>
+			{message && <p>{message}</p>}
+			<p></p>
 			<form onSubmit={handleSubmit}>
 
 				<label htmlFor="nom">Nom</label>
-				<input type="text" id="nom" name="nom" required value={nom} className={styles.input}/>
+				<input type="text" id="nom" name="nom" required defaultValue={nom}  className={styles.input}/>
 
 				<label htmlFor="prenom">prenom</label>
-				<input type="text" id="prenom" name="prenom" required value={prenom} className={styles.input}/>
+				<input type="text" id="prenom" name="prenom" required defaultValue={user.utilisateur_prenom}  className={styles.input}/>
 
 				<label htmlFor="datenaissance">datenaissance</label>
-				<input type="text" id="datenaissance" name="datenaissance" required value={dateNaissance} className={styles.input}/>
+				<input type="text" id="datenaissance" name="datenaissance" required defaultValue={dateNaissance}  className={styles.input}/>
 
 				<label htmlFor="ville">ville</label>
-				<input type="text" id="ville" name="ville" required value={ville} className={styles.input}/>
+				<input type="text" id="ville" name="ville" required defaultValue={ville}  className={styles.input}/>
 
 				<label htmlFor="codepostal">codepostal</label>
-				<input type="text" id="codepostal" name="codepostal" required value={codePostal} className={styles.input}/>
+				<input type="text" id="codepostal" name="codepostal" required defaultValue={codePostal}  className={styles.input}/>
 
 				<label htmlFor="numerorue">numerorue</label>
-				<input type="text" id="numerorue" name="numerorue" required value={numRue} className={styles.input}/>
+				<input type="text" id="numerorue" name="numerorue" required defaultValue={numRue}  className={styles.input}/>
 
 				<label htmlFor="rue">rue</label>
-				<input type="text" id="rue" name="rue" required value={rue} className={styles.input}/>
+				<input type="text" id="rue" name="rue" required defaultValue={rue}  className={styles.input}/>
 
 				<label htmlFor="pays ">pays</label>
-				<input type="text" id="pays " name="pays " required value={pays} className={styles.input}/>
+				<input type="text" id="pays " name="pays " required defaultValue={pays}  className={styles.input}/>
 
 				<button type="submit">
 					Metre à jour
 				</button>
 
-				<p aria-live="polite" role="status">
-					{/*{state?.message}*/}
-				</p>
 			</form>
 		</div>
 	);
--- pages/update-profile.js
+++ pages/update-profile.js
@@ -1,9 +1,23 @@
 import NavbarComponent from "@/components/navbarComponent";
 import UpdateProfilForm from "@/components/users/update-profile";
 
-export async function  getServerSideProps({ query }) {
-	const res = await fetch(process.env.API_ENDPOINT + 'users?id=' + query.id);
-	const user = await res.json()
+export async function  getServerSideProps(context) {
+	const { req } = context;
+
+	if (req.cookies.userId || req.cookies.userId === null
+		|| req.cookies.token || req.cookies.token === null
+) {
+		console.log("problème avec les cookies pour l'id et/ou le token.");
+	}
+
+	const res = await fetch(process.env.API_ENDPOINT + 'users?id=' + req.cookies.userId, {
+		method: 'GET',
+		headers: {
+			Authorization: 'Bearer ' +req.cookies.token
+		}
+	});
+
+	const user = await res.json();
 
 	return {
 		props: {
